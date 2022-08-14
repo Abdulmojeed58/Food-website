1ad8f73580bb36f4b95c95355f79f7bbc9b9442e
abdulmojeed ayoola <mojeedayoola58@gmail.com>
2022-08-14 13:50:19 +0100

    finalTough

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ const $$ = document.querySelectorAll.bind(document)
 
 const menu = $('.fa-solid.fa-bars')
 const nav = $('nav')
+const navLinks = $$('nav li')
 const loader = $('.loader')
 const toTop = $('.top')
 
 menu.addEventListener('click', toggleMenu)
+navLinks.forEach(link => link.addEventListener('click', toggleMenu));
 
 function toggleMenu() {
     nav.classList.toggle('active')
 }
+
 
 
 // ---------LOADER-----------
@@ -24,12 +27,15 @@ setTimeout(()=>{
 
 window.addEventListener('scroll', ()=>{
     if(window.scrollY > 170) {
-        toTop.style.top = 'auto'
-        toTop.style.bottom = '10%'
+        // toTop.style.top = 'auto'
+        toTop.style.top = '90%'
+        // toTop.style.bottom = '10%'
     } else {
         toTop.style.top = '-30%'
-        toTop.style.bottom = 'auto'
+        // toTop.style.bottom = 'auto'
     }
     
 })
+
+
 
